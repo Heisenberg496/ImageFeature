76d767b77123b56b05d8a15828bd3e17e9520eca
Heisenberg496 <ballloveflim4@gmail.com>
2023-08-18 15:35:47 +0700

    first commit

---
+++ app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI,Request
+from hog import hog_des
+import base64
+import numpy as np
+import cv2
+
+app = FastAPI()
+
+def readb64(url):
+    encode_data = url.split(',')[1]
+    nparr = np.fromstring(base64.b64decode(encode_data),np.uint8)
+    img = cv2.imdecode(nparr,cv2.IMREAD_GRAYSCALE)
+    return img
+@app.get('/api/hog')
+async def read_str(request: Request):
+    item = await request.json()
+    item_str = item['img']
+    img = readb64(item_str)
+    hog = hog_des(img)
+    return {"hog":hog.tolist()}
+
+
+
